b0dacb5aee924fbe7c6e51d334e0b32599df469f
perfumesbyacacia-web <perfumesbyacacia@gmail.com>
2025-09-14 21:01:20 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -191,7 +191,7 @@ const Index = () => {
                 variant="outline"
                 size="lg"
                 className="hero-social-button"
-                onClick={() => window.open('https://linkedin.com/in/your-profile', '_blank')}
+                onClick={() => window.open('www.linkedin.com/in/qadri-syed', '_blank')}
               >
                 <Linkedin className="w-5 h-5 mr-2" />
                 LinkedIn
@@ -211,7 +211,7 @@ const Index = () => {
                 variant="outline"
                 size="lg"
                 className="hero-social-button"
-                onClick={() => window.open('#', '_blank')}
+                onClick={() => window.open('https://drive.google.com/file/d/1JsW_mpFO5zPLGBKX7rBcQMctHq1nqNja/view?usp=sharing', '_blank')}
               >
                 <FileText className="w-5 h-5 mr-2" />
                 Resume
